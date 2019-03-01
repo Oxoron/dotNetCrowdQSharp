874a8f75666f56febea8f272e28bf8ad852ba3bb
Alexander Laptev <Alexander.Laptev@devbridge.com>
2019-03-01 14:00:45 +0200

    Add project files.

---
+++ dotNetCrowdQSharp/Driver.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.dotNetCrowdQSharp
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            //TestRandomQubit();
+            //TestRandomGuid();
+            TestBellState();
+            //TestThreeOfFourState();
+            //TestOneBitOfFour();
+
+            Console.ReadLine();            
+        }
+
+
+        static void TestRandomQubit()
+        {  
+            var zeroCounts = 0;
+            var totalCounts = 1000;
+
+            using (var qsim = new QuantumSimulator())
+            {
+                for(int i = 0; i<totalCounts; i++)
+                {
+                    Result random = RandomQubit.Run(qsim).Result;
+                    if (random == Result.Zero) { zeroCounts++; }
+                }                
+            }
+
+            Console.WriteLine($"Zero: {zeroCounts}.\r\n One: {totalCounts-zeroCounts}");
+        }
+
+
+        static void TestRandomGuid()
+        {                 
+            var totalCounts = 10;
+
+            using (var qsim = new QuantumSimulator())
+            {
+                for (int i = 0; i < totalCounts; i++)
+                {
+                    QArray<Result> guid = RandomGuid.Run(qsim, 25).Result;
+                    Console.WriteLine
+                    (
+                        guid.Aggregate
+                        (
+                            seed: String.Empty, 
+                            func: (sum, current) => { return sum + (current == Result.Zero ? "0" : "1"); }
+                        )
+                    );
+                }
+            }            
+        }
+
+
+        static void TestBellState()
+        {
+            var totalCounts = 1000;
+            Dictionary<string, int> resutls = new Dictionary<string, int>()
+            {
+                { "00", 0},
+                { "01", 0},
+                { "10", 0},
+                { "11", 0}
+            };
+
+            using (var qsim = new QuantumSimulator())
+            {
+                for (int i = 0; i < totalCounts; i++)
+                {
+                    (Result, Result) bell = BellState.Run(qsim).Result;
+                    var key = $"{bell.Item1}{bell.Item2}"
+                        .Replace("Zero","0")
+                        .Replace("One", "1");
+
+                    resutls[key] = 1 + resutls[key];
+                }
+
+                foreach (var res in resutls)
+                {
+                    Console.WriteLine($"{res.Key}: {res.Value}");
+                }
+            }            
+        }
+
+         
+        static void TestThreeOfFourState()
+        {
+            var totalCounts = 1000;
+            Dictionary<string, int> resutls = new Dictionary<string, int>()
+            {
+                { "00", 0},
+                { "01", 0},
+                { "10", 0},
+                { "11", 0}
+            };
+
+            using (var qsim = new QuantumSimulator())
+            {
+                for (int i = 0; i < totalCounts; i++)
+                {
+                    var bell = ThreeOfFourState.Run(qsim).Result;
+                    var key = $"{bell.Item1}{bell.Item2}"
+                        .Replace("Zero", "0")
+                        .Replace("One", "1");
+
+                    resutls[key] = 1 + resutls[key];
+                }
+
+                foreach (var res in resutls)
+                {
+                    Console.WriteLine($"{res.Key}: {res.Value}");
+                }
+            }
+        }
+
+        
+        static void TestOneBitOfFour()
+        {
+            var totalCounts = 1000;
+            Dictionary<string, int> resutls = new Dictionary<string, int>(){};
+
+            using (var qsim = new QuantumSimulator())
+            {
+                for (int i = 0; i < totalCounts; i++)
+                {
+                    var fourQubits = OneBitOfFour.Run(qsim).Result;
+                    var key = $"{fourQubits.Item1}{fourQubits.Item2}{fourQubits.Item3}{fourQubits.Item4}"
+                        .Replace("Zero", "0")
+                        .Replace("One", "1");
+
+                    if (resutls.ContainsKey(key))
+                    {
+                        resutls[key] = 1 + resutls[key];
+                    }
+                    else
+                    {
+                        resutls.Add(key, 1);
+                    }
+                }
+
+                foreach (var res in resutls.OrderBy(res => res.Key))
+                {
+                    Console.WriteLine($"{res.Key}: {res.Value}");
+                }
+            }
+        }     
+    }
+}
---
+++ dotNetCrowdQSharp/Operations.qs
@@ -0,0 +1,150 @@
+﻿namespace Quantum.dotNetCrowdQSharp
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+    
+    operation RandomQubit () : (Result)
+    {        
+        mutable res = Zero;
+        
+        using(qubits = Qubit[1])
+        {
+            ResetAll(qubits);
+                
+            H(qubits[0]);
+            set res = M(qubits[0]);                            
+
+
+            ResetAll(qubits); // Always clean after yourself
+        }              
+
+        return res;        
+    }
+
+
+
+    operation RandomGuid (count : Int) : (Result[])
+    {                  
+        mutable res = new Result[count];
+
+        using(qubits = Qubit[count])
+        {
+            ResetAll(qubits);
+                
+            for(i in  0..count-1)
+            {
+                H(qubits[i]);
+                set res[i] = M(qubits[i]);                
+            }                             
+
+            ResetAll(qubits); // Always clean after yourself
+        }              
+
+        return res;              
+    }
+
+
+
+    operation BellState () : (Result, Result)
+    {                  
+        mutable res0 = Zero;
+        mutable res1 = Zero;
+
+        using(qubits = Qubit[2])
+        {
+            ResetAll(qubits);
+                
+            H(qubits[0]);
+            CNOT(qubits[0], qubits[1]);
+
+            set res0 = M(qubits[0]);
+            set res1 = M(qubits[1]);
+
+            ResetAll(qubits); // Always clean after yourself
+        }              
+
+        return (res0, res1);              
+    }
+
+
+
+
+    operation ThreeOfFourState () : (Result, Result)
+    {                  
+        mutable res0 = Zero;
+        mutable res1 = Zero;
+
+        using(qubits = Qubit[2])
+        {
+            ResetAll(qubits);
+                
+            H(qubits[0]);
+            (Controlled (H)) 
+            (
+                [qubits[0]], // Controllers array
+                qubits[1] // Arguments
+            );
+            
+
+            set res0 = M(qubits[0]);
+            set res1 = M(qubits[1]);
+
+            ResetAll(qubits); // Always clean after yourself
+        }              
+
+        return (res0, res1);              
+    }
+
+
+
+    operation OneBitOfFour () : (Result, Result, Result, Result)
+    {           
+        // Schema: https://algassert.com/quirk#circuit={%22cols%22:[[%22H%22,1,%22H%22],[%22%E2%80%A2%22,1,%22%E2%80%A2%22,%22X%22],[%22%E2%97%A6%22,%22X%22,%22%E2%97%A6%22],[%22X%22,1,%22X%22,%22%E2%80%A2%22]]}
+        mutable res0 = Zero;
+        mutable res1 = Zero;
+        mutable res2 = Zero;
+        mutable res3 = Zero;
+
+        using(qubits = Qubit[4])
+        {
+            ResetAll(qubits);
+                
+            // Step 1
+            H(qubits[0]);
+            H(qubits[2]);
+
+            // Step 2
+            (Controlled (X)) 
+            (
+                [qubits[0], qubits[2]], 
+                qubits[3] 
+            );            
+
+            // Step 3
+            X(qubits[0]);
+            X(qubits[2]);
+            (Controlled (X)) 
+            (
+                [qubits[0], qubits[2]], 
+                qubits[1] 
+            );
+            X(qubits[0]);
+            X(qubits[2]);
+
+
+            // Step 4
+            CNOT(qubits[3], qubits[0]);
+            CNOT(qubits[3], qubits[2]);
+            
+
+            set res0 = M(qubits[0]);
+            set res1 = M(qubits[1]);
+            set res2 = M(qubits[2]);
+            set res3 = M(qubits[3]);
+
+            ResetAll(qubits); // Always clean after yourself
+        }              
+
+        return (res0, res1, res2, res3);              
+    }
+}
